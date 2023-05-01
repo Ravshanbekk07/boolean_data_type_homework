4b5ba6e3760104968483c76e98aa21c2230b67c3
Ravshanbekk07 <ramazonof07@gmail.com>
2023-05-01 11:16:06 +0500

    donee

--- bool04.py
+++ bool04.py
@@ -7,4 +7,21 @@ def main(a):
         bool
     """
     # Write your code here
-    return 
+    return a < 0
+
+v = main(-3)
+print(v)
+
+def main(b):
+    """
+    check the following statement "The variable "b" is positive"
+    Args:
+        b: int
+    Returns:
+        bool
+    """
+    # Write your code here
+    return b<0
+
+v  = main(8)
+print(v)
